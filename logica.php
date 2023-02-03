88fcaed3b5c57fb274a4fec0f49bf62f49c104de
FirminoJFSilva <firminoferreirasilva@gmail.com>
2023-02-02 21:55:35 -0300

    first commit

---
+++ logica.php
@@ -0,0 +1,18 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+    
+    $para = 'seu-email@exemplo.com';
+    $assunto = 'Novo contato do site';
+    $mensagem = "Nome: $nome\n\nEmail: $email\n\nMensagem: $mensagem";
+    $cabecalhos = "From: $nome <$email>";
+    
+    if (mail($para, $assunto, $mensagem, $cabecalhos)) {
+      echo 'Mensagem enviada com sucesso!';
+    } else {
+      echo 'Erro ao enviar mensagem';
+    }
+  }
+?>
