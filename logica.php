05f9b12320f232e9084fd6d64a4a1c143ba26b93
FirminoJFSilva <firminoferreirasilva@gmail.com>
2023-02-02 22:00:01 -0300

    segundo

--- logica.php
+++ logica.php
@@ -4,7 +4,7 @@
     $email = $_POST['email'];
     $mensagem = $_POST['mensagem'];
     
-    $para = 'seu-email@exemplo.com';
+    $para = 'firminoferreirasilva@gmail.com';
     $assunto = 'Novo contato do site';
     $mensagem = "Nome: $nome\n\nEmail: $email\n\nMensagem: $mensagem";
     $cabecalhos = "From: $nome <$email>";
